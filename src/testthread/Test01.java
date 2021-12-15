3a43b4b6785c871d658486ad0a554f4f261c3b23
xiedongting <2410378066@qq.com>
2021-12-15 10:05:02 +0800

    添加了一行

--- src/testthread/Test01.java
+++ src/testthread/Test01.java
@@ -4,5 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println(123);
+        System.out.println("111");
     }
 }
