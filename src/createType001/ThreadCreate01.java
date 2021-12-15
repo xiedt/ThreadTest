948d33050f8a5f6fc1c86166c4436ffb4dd15a71
xiedongting <2410378066@qq.com>
2021-12-15 15:35:09 +0800

    写了第一种

--- src/createType001/ThreadCreate01.java
+++ src/createType001/ThreadCreate01.java
@@ -1,7 +1,23 @@
 package createType001;
 
+/**
+ * 创建线程的第一种方式，继承Thread类，重写run方法
+ *
+ */
 public class ThreadCreate01 {
-    public static void main(String[] args) {
 
+        //main函数是一个主线程
+        public static void main(String[] args) throws InterruptedException {
+            MyThread01 myThread01 = new MyThread01();
+            myThread01.start();
+            //Thread.sleep(1000);
+            System.out.println("执行主线程");
+        }
     }
-}
+
+    class MyThread01 extends Thread{
+        @Override
+        public void run(){
+            System.out.println("执行子线程");
+        }
+    }
