f089405f25475059c76b5c12957df12afe26bc8d
xiedongting <2410378066@qq.com>
2021-12-15 10:23:56 +0800

    ceshi

--- src/testthread/Test01.java
+++ src/testthread/Test01.java
@@ -4,6 +4,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("123");
         System.out.println(123);
-        System.out.println("111");
+        System.out.println(222);
     }
 }
