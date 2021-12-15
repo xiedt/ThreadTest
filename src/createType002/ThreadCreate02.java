179c286ea79b6bbab9ad814749c435bb82bcba24
xiedongting <2410378066@qq.com>
2021-12-15 16:25:58 +0800

    第二和第三

--- src/createType002/ThreadCreate02.java
+++ src/createType002/ThreadCreate02.java
@@ -1,4 +1,31 @@
 package createType002;
 
+/**
+ * 线程类只是实现了Runable接口，还可以继承其他的类。
+ * 可以多个线程共享同一个目标对象，所以非常适合多个相同线程来处理同一份资源的情况。
+ */
 public class ThreadCreate02 {
+    public static void main(String[] args) {
+        Runnable runnable = new MyThread02();
+        Thread thread = new Thread(runnable);
+        thread.start();
+        System.out.println("执行主线程");
+    }
 }
+class MyThread02 implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("执行子线程");
+    }
+}
+/**
+ * Runnable可以实现资源共享但Thread不能原因：
+ *
+ * 因为一个线程只能启动一次，通过Thread实现线程时，线程和线程所要执行的任务是捆绑在一起的。
+ * 也就使得一个任务只能启动一个线程，不同的线程执行的任务是不相同的，所以没有必要，也不能让两个线程共享彼此任务中的资源。
+ *
+ * 一个任务可以启动多个线程，通过Runnable方式实现的线程，实际是开辟一个线程，将任务传递进去，
+ * 由此线程执行。可以实例化多个 Thread对象，将同一任务传递进去，也就是一个任务可以启动多个线程来执行它。
+ * 这些线程执行的是同一个任务，所以他们的资源是共享。
+ */
--- src/createType003/ThreadCreate03.java
+++ src/createType003/ThreadCreate03.java
@@ -1,4 +1,27 @@
 package createType003;
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
 
 public class ThreadCreate03 {
+    public static void main(String[] args) {
+        Callable callable = new MyCallable();
+        for (int i = 0; i < 10; i++) {
+            FutureTask task = new FutureTask(callable);
+            new Thread(task,"子线程"+ i).start();
+            try {
+                //获取子线程的返回值
+                System.out.println("子线程返回值："+task.get() + "\n");
+            }  catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+ class MyCallable implements Callable {
+    int i = 0;
+    @Override
+    public Object call() throws Exception {
+        System.out.println(Thread.currentThread().getName()+"  i的值："+ i);
+        return i++; //call方法可以有返回值
+    }
 }
